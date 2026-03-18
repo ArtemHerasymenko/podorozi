fd48f69f8103913670d2f762670ba89a1312d879
Artem <agerasimencko34@gmail.com>
2026-03-18 23:23:15 +0200

    import book trip

--- handlers/passenger.py
+++ handlers/passenger.py
@@ -1,7 +1,7 @@
 from aiogram import Router, types
 from aiogram.fsm.context import FSMContext
 from states.passenger_states import PassengerStates
-from database import search_trips
+from database import search_trips, book_trip
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 from keyboards.city_kb import cities_keyboard
 from aiogram.types import ReplyKeyboardRemove
