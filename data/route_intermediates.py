890b2a0c5a803332d91a2df9fd9b73b136d2f579
Artem <agerasimencko34@gmail.com>
2026-05-20 23:34:31 +0300

    do not stop on first route

--- data/route_intermediates.py
+++ data/route_intermediates.py
@@ -11,21 +11,25 @@ def get_search_city_pairs(from_city: str, to_city: str) -> tuple[list[str], list
       extra_from_cities = [1]  (earlier starts that go to `to_city`)
       extra_to_cities   = [5]  (later ends that start from `from_city`)
     """
+    extra_from, extra_to = set(), set()
     for route in TYPICAL_ROUTES:
         if from_city in route and to_city in route:
             i, j = route.index(from_city), route.index(to_city)
             if i < j:
-                return route[:i], route[j + 1:]
+                extra_from.update(route[:i])
+                extra_to.update(route[j + 1:])
             else:
-                return route[i + 1:], route[:j]
-    return [], []
+                extra_from.update(route[i + 1:])
+                extra_to.update(route[:j])
+    return list(extra_from), list(extra_to)
 
 def get_intermediates(from_city: str, to_city: str) -> list[str]:
+    result = set()
     for route in TYPICAL_ROUTES:
         if from_city in route and to_city in route:
             i, j = route.index(from_city), route.index(to_city)
             if i < j:
-                return route[i + 1:j]
+                result.update(route[i + 1:j])
             else:
-                return route[j + 1:i][::-1]
-    return []
+                result.update(route[j + 1:i])
+    return list(result)
